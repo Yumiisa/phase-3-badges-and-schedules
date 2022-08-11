80a88d894c59a4e85dc9fbfb9fbf295424a6ed0f
Gaudencia <conny@students.uonbi.ac.ke>
2022-08-11 21:54:11 +0300

    commit tree 796a61b67d92c823ce42c7d666831419285f4b8b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker name
+    
+"Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+    greetings=[]
+names.each do |name|
+    greetings << badge_maker(name)
+end
+greetings
+end
+def assign_rooms speakers
+    greet=[]
+    speakers.each_with_index do |speaker, index|
+        greet << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+        
+    end
+    return greet
+end
+def printer(attendees)
+    resultOne=batch_badge_creator(attendees)
+    resultOne.each do |x|
+        puts x
+    end
+    result=assign_rooms(attendees)
+    result.each do |x|
+        puts x
+    end
+end
